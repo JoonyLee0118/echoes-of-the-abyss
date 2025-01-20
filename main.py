b519e0e0a08852b68ee39d713c8dd9ef9bde2f31
JoonyLee0118 <chunhuilee0118@gmail.com>
2025-01-20 12:36:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    screen.fill("purple")
 
     # RENDER YOUR GAME HERE
 
